f12e2de0e2b6d8e752ed90012f84456c3c9a74ca
João L. Barboza <105928845+valladev@users.noreply.github.com>
2024-01-17 21:30:15 -0300

    :sparkles: feat: Adicionando anotações do 'Bean Validation' no Record `DadosCadastroMedicos` e excutando uma nova Migration.

--- src/main/java/medvoll/api/controller/MedicoController.java
+++ src/main/java/medvoll/api/controller/MedicoController.java
@@ -1,9 +1,11 @@
 package medvoll.api.controller;
 
+import jakarta.validation.Valid;
 import medvoll.api.medico.DadosCadastroMedicos;
 import medvoll.api.medico.Medico;
 import medvoll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +19,9 @@ public class MedicoController {
     private MedicoRepository repository;
 
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedicos dados) {
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedicos dados) {
         repository.save(new Medico(dados));
     }
+
 }
