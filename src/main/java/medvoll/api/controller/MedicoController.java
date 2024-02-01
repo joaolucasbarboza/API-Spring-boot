91d679a0aec1a8ab706ff82b438963c93d1cd9d9
João L. Barboza <105928845+valladev@users.noreply.github.com>
2024-02-01 15:55:21 -0300

    :sparkles: feat.: Listagem de medicos, com ordenação de paginação.

--- src/main/java/medvoll/api/controller/MedicoController.java
+++ src/main/java/medvoll/api/controller/MedicoController.java
@@ -2,14 +2,17 @@ package medvoll.api.controller;
 
 import jakarta.validation.Valid;
 import medvoll.api.medico.DadosCadastroMedicos;
+import medvoll.api.medico.DadosListagemMedico;
 import medvoll.api.medico.Medico;
 import medvoll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -22,6 +25,11 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedicos dados) {
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = { "nome" }) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 
 }
