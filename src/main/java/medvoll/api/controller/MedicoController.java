09a000778f60edffdafea349844400f3f2e54a50
João L. Barboza <105928845+valladev@users.noreply.github.com>
2024-02-03 15:52:23 -0300

    :sparkles: feat: exclusão logica de um medico por meio do `Id` no endpoint da rota HTTP.

--- src/main/java/medvoll/api/controller/MedicoController.java
+++ src/main/java/medvoll/api/controller/MedicoController.java
@@ -26,7 +26,7 @@ public class MedicoController {
 
     @GetMapping
     public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = { "nome" }) Pageable paginacao) {
-        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+        return repository.findAllByStatusTrue(paginacao).map(DadosListagemMedico::new);
     }
 
     @PutMapping
@@ -34,6 +34,13 @@ public class MedicoController {
     public void atualizar(@RequestBody @Valid DadosAtualizarMedicos dados) {
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
     }
 
 }
---
+++ src/main/resources/db/migration/V4__update-table-medicos-add-column-status.sql
@@ -0,0 +1,2 @@
+alter table medicos add status tinyint;
+update medicos set status = 1;
