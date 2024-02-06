745627a2e01108e72800b88fe4abe6276cc1129d
João L. Barboza <105928845+valladev@users.noreply.github.com>
2024-02-06 13:38:33 -0300

    adicionando padrões de retorno da API sobre cada verbo e adicionando a busca por Ìd`. no

--- src/main/java/medvoll/api/controller/PacienteController.java
+++ src/main/java/medvoll/api/controller/PacienteController.java
@@ -1,13 +1,15 @@
 package medvoll.api.controller;
 
 import jakarta.validation.Valid;
-import medvoll.api.paciente.*;
+import medvoll.api.domain.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -18,26 +20,45 @@ public class PacienteController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroPacientes dados) {
-        repository.save(new Paciente(dados));
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroPacientes dados, UriComponentsBuilder uriBuilder) {
+        var paciente = new Paciente(dados);
+        repository.save(paciente);
+
+        var uri = uriBuilder.path("/pacientes/{id}").buildAndExpand(paciente.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoPaciente(paciente));
     }
 
     @GetMapping
-    public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, sort = { "nome" })Pageable paginacao) {
-        return repository.findAllByStatusTrue(paginacao).map(DadosListagemPaciente::new);
+    public ResponseEntity<Page<DadosListagemPaciente>> listar(@PageableDefault(size = 10, sort = { "nome" })Pageable paginacao) {
+        var page = repository.findAllByStatusTrue(paginacao).map(DadosListagemPaciente::new);
+
+        return ResponseEntity.ok(page);
     }
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizarPacientes dados) {
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizarPacientes dados) {
         var paciente = repository.getReferenceById(dados.id());
         paciente.atualizarInformacoes(dados);
+
+        return ResponseEntity.ok(new DadosDetalhamentoPaciente(paciente));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void excluir(@PathVariable Long id) {
+    public ResponseEntity excluir(@PathVariable Long id) {
         var paciente = repository.getReferenceById(id);
         paciente.excluir();
+
+        return ResponseEntity.noContent().build();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        var paciente = repository.getReferenceById(id);
+
+        return ResponseEntity.ok(new DadosDetalhamentoPaciente(paciente));
+    }
+
 }
---
+++ src/main/java/medvoll/api/domain/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,17 @@
+package medvoll.api.domain.paciente;
+
+import medvoll.api.domain.endereco.Endereco;
+
+public record DadosDetalhamentoPaciente(
+        Long id,
+        String nome,
+        String email,
+        String telefone,
+        String cpf,
+        Endereco endereco) {
+
+    public DadosDetalhamentoPaciente(Paciente paciente) {
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getTelefone(), paciente.getCpf(),
+                paciente.getEndereco());
+    }
+}
