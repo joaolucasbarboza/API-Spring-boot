b3712c48e6e3311780b4dcc93fb8b4d3351e5aa9
João L. Barboza <105928845+valladev@users.noreply.github.com>
2024-03-19 09:32:43 -0300

    :sparkles: feat: Criando a classe a tebela da funcionalidade de `Consutlas`.

---
+++ src/main/java/medvoll/api/domain/consulta/DadosAgendamento.java
@@ -0,0 +1,17 @@
+package medvoll.api.domain.consulta;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record DadosAgendamento(
+        Long idMedico,
+
+        @NotNull
+        Long idPaciente,
+
+        @NotNull
+        @Future
+        LocalDateTime data) {
+}
