65787088bc75e2b630d78918598a8530a7766a64
João L. Barboza <joaolucasbarbozaa@gmail.com>
2024-05-14 22:22:31 -0300

    feat: Criação da classe de detalhamento da `Recepcionista`.

---
+++ src/main/java/medvoll/api/domain/recepcionista/DadosDetalhamentoRecepcionista.java
@@ -0,0 +1,22 @@
+package medvoll.api.domain.recepcionista;
+
+import medvoll.api.domain.endereco.Endereco;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public record DadosDetalhamentoRecepcionista(
+        Long id,
+        String nome,
+        String telefone,
+        LocalDate dataContratacao,
+        LocalDate dataNascimento,
+        Float salario,
+        String cpf,
+        Endereco endereco,
+        Boolean status) {
+
+    public DadosDetalhamentoRecepcionista(RecepcionistaEntity recepcionista) {
+        this(recepcionista.getId(), recepcionista.getNome(), recepcionista.getTelefone(), recepcionista.getDataContratacao(), recepcionista.getDataNascimento(), recepcionista.getSalario(), recepcionista.getCpf(), recepcionista.getEndereco(), recepcionista.getStatus());
+    }
+}
