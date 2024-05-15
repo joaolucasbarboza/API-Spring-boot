640edc2fd39aea19bcf016cc78d70d4106ad3a03
João L. Barboza <joaolucasbarbozaa@gmail.com>
2024-05-14 22:24:56 -0300

    Refactor: Refatoração dos imports na classe `MedicoController`.

--- src/main/java/medvoll/api/controller/MedicoController.java
+++ src/main/java/medvoll/api/controller/MedicoController.java
@@ -2,14 +2,26 @@ package medvoll.api.controller;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
-import medvoll.api.domain.medico.*;
+import medvoll.api.domain.medico.DadosAtualizarMedicos;
+import medvoll.api.domain.medico.DadosCadastroMedicos;
+import medvoll.api.domain.medico.DadosDatalhamentoMedico;
+import medvoll.api.domain.medico.DadosListagemMedico;
+import medvoll.api.domain.medico.Medico;
+import medvoll.api.domain.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
--- src/main/java/medvoll/api/domain/remedio/DadosCadastroRemedio.java
+++ src/main/java/medvoll/api/domain/remedio/DadosCadastroRemedio.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import medvoll.api.domain.consulta.FormaPagamento;
 
 public record DadosCadastroRemedio(
 
