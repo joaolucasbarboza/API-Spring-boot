807769240d79dc44db71f168808684bec88c5203
João L. Barboza <joaolucasbarbozaa@gmail.com>
2024-05-14 22:21:38 -0300

    feat: Criação da interface `RecepcionistaRepository`.

---
+++ src/main/java/medvoll/api/domain/recepcionista/RecepcionistaRepository.java
@@ -0,0 +1,6 @@
+package medvoll.api.domain.recepcionista;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecepcionistaRepository extends JpaRepository<RecepcionistaEntity, Long> {
+}
