a5e3694e2940eeb4a6ccdf3f1659a7e9b748e67e
João L. Barboza <joaolucasbarbozaa@gmail.com>
2024-06-03 14:49:49 -0300

    configuration: Alterando a URL do cors.

--- src/main/java/medvoll/api/config/CorsConfiguration.java
+++ src/main/java/medvoll/api/config/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://172.100.122.157:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
