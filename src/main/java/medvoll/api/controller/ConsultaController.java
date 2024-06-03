0d4766cba1b74f87b260784c38174eaf0bc11b91
João L. Barboza <joaolucasbarbozaa@gmail.com>
2024-06-03 14:48:57 -0300

    feat: Funcionalidades de consultar as consultas por dia, semana e mês.

--- src/main/java/medvoll/api/controller/ConsultaController.java
+++ src/main/java/medvoll/api/controller/ConsultaController.java
@@ -2,17 +2,30 @@ package medvoll.api.controller;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
-import medvoll.api.domain.consulta.*;
+import medvoll.api.domain.consulta.AgendarConsultas;
+import medvoll.api.domain.consulta.ConsultaEntity;
+import medvoll.api.domain.consulta.ConsultaRepository;
+import medvoll.api.domain.consulta.DadosAgendamento;
+import medvoll.api.domain.consulta.DadosAtualizarConsulta;
+import medvoll.api.domain.consulta.DadosCancelamentoConsulta;
+import medvoll.api.domain.consulta.DadosDetalhamentoConsulta;
+import medvoll.api.domain.consulta.DadosListagemConsulta;
 import medvoll.api.domain.remedio.RemedioEntity;
 import medvoll.api.domain.remedio.RemedioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.TemporalAdjusters;
 import java.util.List;
 
 @RestController
@@ -41,6 +54,42 @@ public class ConsultaController {
     public ResponseEntity<Page<DadosListagemConsulta>> listar(@PageableDefault() Pageable pageable) {
 
         return ResponseEntity.ok(consultaRepository.findAll(pageable).map(DadosListagemConsulta::new));
+    }
+
+    @GetMapping("/consultas-hoje")
+    public ResponseEntity<List<ConsultaEntity>> listarConsultasHoje() {
+
+        LocalDate today = LocalDate.now();
+
+        LocalDateTime inicioDia = today.atStartOfDay();
+
+        LocalDateTime fimDia = today.atTime(LocalTime.MAX);
+
+        List<ConsultaEntity> consultas = consultaRepository.findByDataBetween(inicioDia, fimDia, Sort.by("data").ascending());
+
+        return ResponseEntity.ok(consultas);
+    }
+
+    @GetMapping("/consultas-semana")
+    public ResponseEntity<List<ConsultaEntity>> listarConsultasDaSemana() {
+
+        LocalDateTime inicioDaSemana = LocalDateTime.now().with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY)).toLocalDate().atStartOfDay();
+        LocalDateTime fimDaSemana = LocalDateTime.now().with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY)).toLocalDate().atTime(LocalTime.MAX);
+
+        List<ConsultaEntity> consultas = consultaRepository.findByDataBetween(inicioDaSemana, fimDaSemana, Sort.by("data").ascending());
+
+        return ResponseEntity.ok(consultas);
+    }
+
+    @GetMapping("/consultas-mes")
+    public ResponseEntity<List<ConsultaEntity>> listarConsultasDoMes() {
+
+        LocalDateTime inicioDoMes = LocalDate.now().with(TemporalAdjusters.firstDayOfMonth()).atStartOfDay();
+        LocalDateTime fimDoMes = LocalDate.now().with(TemporalAdjusters.lastDayOfMonth()).atTime(LocalTime.MAX);
+
+        List<ConsultaEntity> consultas = consultaRepository.findByDataBetween(inicioDoMes, fimDoMes, Sort.by("data").ascending());
+
+        return ResponseEntity.ok(consultas);
     }
 
     @PutMapping("/{id}")
--- src/main/java/medvoll/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/medvoll/api/domain/consulta/ConsultaRepository.java
@@ -1,12 +1,16 @@
 package medvoll.api.domain.consulta;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public interface ConsultaRepository extends JpaRepository<ConsultaEntity, Long> {
 
     boolean existsByUsuarioIdAndDataBetween(Long idUsuario, LocalDateTime primeiroHorario, LocalDateTime ultimoHorario);
 
     boolean existsByMedicoIdAndData(Long idMedico, LocalDateTime data);
+
+    List<ConsultaEntity> findByDataBetween(LocalDateTime inicioDaSemana, LocalDateTime fimDaSemana, Sort data);
 }
